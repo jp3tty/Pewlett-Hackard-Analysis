92029e2cfbc954be355f8720a4446372a21e5e40
jp3tty <jeremy.m.petty@gmail.com>
2021-11-04 11:11:38 -0700

    Commit

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,56 @@
+-- Deliverable 1: The number of retiring Employees by Title
+-- Following instructions
+SELECT e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   t.title,
+	   t.from_date,
+	   t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-13')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+				   first_name,
+				   last_name,
+				   title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, title DESC;
+
+SELECT * FROM unique_titles
+
+-- Retrieve the number of employees from their most recent job title who are about to retire
+SELECT COUNT(urt.emp_no),
+urt.title
+INTO retiring_titles
+FROM unique_titles AS urt
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM retiring_titles
+
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+-- Following Instructions (Write a query to create Eligibility)
+SELECT DISTINCT ON(e.emp_no)
+				   e.emp_no,
+				   e.first_name,
+				   e.last_name,
+				   e.birth_date,
+				   de.from_date,
+				   de.to_date,
+				   t.title
+-- INTO mentorship_eligibility
+FROM employees AS e
+LEFT OUTER JOIN dept_emp AS de
+ON (e.emp_no=de.emp_no)
+LEFT OUTER JOIN titles AS t
+ON (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Pewlett-Hackard-Analysis
+
+## Overview
+
+The purpose of the new analysis is well defined.
+
+## Results
+
+There is a bulleted list with four major points from the two analysis deliverables.
+
+* Image of the ERD
+
+Include images from pgadmin4.
+
+Deliverable 1
+* Image of the count of retiring per department.
+* Image of the retirement_titles.csv
+* Image of the unique_titles.csv
+* Image of the retiring_titles.csv
+
+Deliverable 2
+* Image of mentorship_eligibility.csv
+* Count of potential employees eligible for mentorship
+
+## Summary
+
+The summary addresses the two questions and contains two additional queries or tables that may provide more insight.
+
+* How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+* Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
